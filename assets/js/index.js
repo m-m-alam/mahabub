3ac1acc7cbb873e8b90d0b51c9ad066f8d41cafc
DESKTOP-RFF4P0C\malam <mahabub@vonome.com>
2022-03-19 22:47:11 +0600

    code update

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+var options = {
+    //strings: ['I\'m Mahabub Alam.', '&amp; I\'m a Fullstack Developer.'],
+    strings: ['I\'m Mahabub Alam. I\'m a Fullstack Developer.'],
+    typeSpeed: 40,
+    smartBackspace: true,
+    loop: true,
+  };
+  
+  var typed = new Typed('.element', options);
